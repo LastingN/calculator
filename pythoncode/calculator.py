e3a02f7ae643e89616ecb3bdddb2b8525138180a
yunbosheng <bosheng.yun@mosoink.com>
2020-12-23 17:51:49 +0800

    计算器用例

---
+++ pythoncode/calculator.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# @Time     :2020/12/23 10:25 上午
+# @Author   :yun bosheng
+# @File     :calculator.py
+
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def sub(self, a, b):
+        return a - b
+
+    def mul(self, a, b):
+        return a * b
+
+    def div(self, a, b):
+        return a / b
